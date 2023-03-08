2cabca26be994b9b30a07dd4c303b4ba8d7ef09a
ritualFire <jpelaez456@gmail.com>
2023-03-08 03:37:38 +0000

    ArrayAbsoluteValue

--- src/main/java/ArrayAbsoluteValue.java
+++ src/main/java/ArrayAbsoluteValue.java
@@ -9,6 +9,10 @@ public class ArrayAbsoluteValue {
      * @return the absolute value array of nums.
      */
     public int[] getArrayAbs(int[] nums){
-        return null;
+        int[] absNum = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            absNum[i]=Math.abs(nums[i]);
+        }
+        return absNum;
     }
 }
